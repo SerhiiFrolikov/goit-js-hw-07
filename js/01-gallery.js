d5b9fe5d06698043964ed7dd385265ba3fac55b7
SerhiiFrolikov <116305785+SerhiiFrolikov@users.noreply.github.com>
2023-04-01 22:52:36 +0300

    task 1 and 2 DONE

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createPictureGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function createPictureGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return ` 
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+         })
+        .join('');  
+}
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryImageEl) {
+        return; 
+    }
+    const currentImageEl = evt.target.closest('.gallery__image');
+    const { source } = currentImageEl.dataset;
+  
+    const data = galleryItems.find(({ original }) => original === source);
+    const instance = basicLightbox.create(`
+   <div>
+   <img src="${data.original}" alt="${data.description}">
+   </div>
+    `);
+    
+    const closeBtnEscape = (evt) => {
+        if (evt.key === "Escape") {
+            instance.close();
+            document.removeEventListener("keydown", closeBtnEscape);
+        }
+    };
+
+    document.addEventListener("keydown", closeBtnEscape);
+    instance.show();
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createPictureGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function createPictureGalleryMarkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return ` 
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+         })
+        .join('');  
+}
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImageEl = evt.target.classList.contains('gallery__image');
+    if (!isGalleryImageEl) {
+        return;
+    }
+}
+    const lightbox = new SimpleLightbox('.gallery a', {
+        captions: true,
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        captionsData: "alt",
+    });
